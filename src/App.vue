3f0fab1efceda449ef3092e5e309260f73736781
asagiman <lagavulin.jb.dc@gmail.com>
2022-08-28 12:33:33 +0900

    1

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,140 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import Card from './components/Card.vue'
+
+const items = ref([
+  {
+    id: 1,
+    name: 'アボカドソースバケット',
+    description:
+      '刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。',
+    price: 480,
+    image: '/images/item1.jpg',
+    soldOut: false,
+    selected: false
+  },
+  {
+    id: 2,
+    name: 'あの日夢見たホットケーキ',
+    description:
+      '子供のころに食べたかった、あのホットケーキを再現しました。素朴でどこか懐かしい味をどうぞ。',
+    price: 1180,
+    image: '/images/item2.jpg',
+    soldOut: false,
+    selected: false
+  },
+  {
+    id: 3,
+    name: 'HOP WTR',
+    description:
+      'ロサンゼルス生まれのスパークリングウォーター。ノンカロリー、ノンアルコールの新感覚飲料です。',
+    price: 320,
+    image: '/images/item3.jpg',
+    soldOut: true,
+    selected: false
+  },
+  {
+    id: 4,
+    name: 'チーズフレンチフライ',
+    description:
+      'イタリア産チーズをたっぷりかけたアツアツのフレンチフライ。みんな大好きな一品です。',
+    price: 670,
+    image: '/images/item4.jpg',
+    soldOut: false,
+    selected: false
+  }
+])
+
+function changeSoldOut(id) {
+  const pickElm = items.value.find(item => item.id == id)
+  pickElm.soldOut = true
+}
 </script>
 
 <template>
- <header class="header">
-  <img 
-    src="/images/logo.svg"
-    alt="">
-    <h1>Vue.js ハンズオン</h1>
- </header>
-
- <main class="main">
-  <div class="item">
-    <div class="thumbnail">
-      <img 
-        :src="item.image"
-        alt="">
-    </div>
-    <div class="description">
-      <h2>{{ item.name }}</h2>
-      <p>{{ item.description }}</p>
-      <span>¥<span class="price">{{ item.price }}</span></span>
-    </div>
-  </div>
- </main>
+  <header class="header">
+    <img 
+      src="/images/logo.svg"
+      alt="">
+      <h1>Vue.js ハンズオン</h1>
+  </header>
+    <main class="main">
+        <template
+          v-for="item in items"
+          :key="item.id">
+          <div
+            v-if="!item.soldOut"
+            class="item"
+            :class="{ 'selected-item': item.selected }"
+            @click="item.selected = !item.selected">
+            <Card 
+              :id="item.id"
+              :image="item.image"
+              :name="item.name"
+              :description="item.description"
+              :price="item.price"
+              @sold-out="changeSoldOut"/>
+          </div>    
+      </template>
+    </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+.selected-item {
+  background-color: #e3f2fd;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+body {
+  font-family: sans-serif;
+  margin: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#app {
+  width: 90%;
+  margin: 0 5%;
+  color: #242424;
 }
+
+.header {
+  display: flex;
+  align-content: center;
+  align-items: center;
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+
+.header > img {
+  width: 100px;
+  height: 100px;
+  margin-right: 20px;
+}
+
+.header > h1 {
+  font-size: 80px;
+  font-weight: bold;
+  line-height: 80px;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.main {
+  display: grid;
+  grid-template-columns: 3fr 3fr 3fr 3fr;
+  column-gap: 24px;
+  row-gap: 24px;
+}
+
+.item {
+  padding: 10px;
+  cursor: pointer;
+}
+
+.item:hover {
+  transition: 0.2s transform ease-out;
+  transform: scale(1.05);
+}
+
+
 </style>
