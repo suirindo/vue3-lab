36e868d1d1ba8db32481b67d7d3fe896e7c668c3
asagiman <lagavulin.jb.dc@gmail.com>
2022-08-28 10:03:05 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+ <header class="header">
+  <img 
+    src="/images/logo.svg"
+    alt="">
+    <h1>Vue.js ハンズオン</h1>
+ </header>
+
+ <main class="main">
+  <div class="item">
+    <div class="thumbnail">
+      <img 
+        :src="item.image"
+        alt="">
+    </div>
+    <div class="description">
+      <h2>{{ item.name }}</h2>
+      <p>{{ item.description }}</p>
+      <span>¥<span class="price">{{ item.price }}</span></span>
+    </div>
+  </div>
+ </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
